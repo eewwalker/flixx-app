44c9fafa0a903fbacca7abbac9dfe2eac1dbb460
Emily Walker <ewalker415@gmail.com>
2023-10-28 10:57:14 -0700

    tv-show details

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const global = {
 const popularMovies = document.querySelector("#popular-movies");
 const tvShows = document.querySelector("#popular-shows");
 const movieDetails = document.querySelector("#movie-details");
+const showDetails = document.querySelector("#show-details");
 
 async function displayPopularMovies() {
   const { results } = await fetchAPIData("movie/popular");
@@ -109,7 +110,7 @@ async function displayMovieDetails() {
 
 function displayBackDrop(type, path) {
   const backDropDiv = document.createElement("div");
-  backDropDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${path})`;
+  backDropDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${path})`;
   backDropDiv.style.backgroundSize = "cover";
   backDropDiv.style.backgroundPosition = "center";
   backDropDiv.style.backgroundRepeat = "no-repeat";
@@ -120,7 +121,9 @@ function displayBackDrop(type, path) {
   backDropDiv.style.left = "0";
   backDropDiv.style.zIndex = "-1";
   backDropDiv.style.opacity = "0.2";
-  type === 'movie' ? movieDetails.append(backDropDiv) : tvShows.append(backDropDiv);
+  type === "movie"
+    ? movieDetails.append(backDropDiv)
+    : showDetails.append(backDropDiv);
 }
 function addCommasToNumber(number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -160,6 +163,70 @@ async function displayTvShows() {
   console.log(results);
 }
 
+async function displayTvDetails() {
+  const showId = window.location.search.split("=")[1];
+  const show = await fetchAPIData(`tv/${showId}`);
+  displayBackDrop("show", show.backdrop_path);
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <div class="details-top">
+          <div>
+          ${
+            show.poster_path
+              ? `<img
+              src='https://image.tmdb.org/t/p/w500${show.poster_path}'
+              class="card-img-top"
+              alt=${show.name}
+            />
+            `
+              : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt=${show.name}
+            />`
+          }
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+             ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">First Air Date: ${show.first_air_date}</p>
+            <p>
+             ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+            ${show.genres.map((s) => `<li>${s.name}</li>`).join("")}
+             
+            </ul>
+            <a href=${
+              show.homepage
+            } target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number Of Episodes:</span> ${
+              show.number_of_episodes
+            }</li>
+            <li>
+              <span class="text-secondary">Last Episode To Air:</span> ${
+                show.last_episode_to_air.name
+              }
+            </li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+          ${show.production_companies.map((s) => ` <span>${s.name}</span>`)}
+      </div>
+        </div>`;
+  showDetails.append(div);
+}
+
 const showSpinner = () => {
   document.querySelector(".spinner").classList.add("show");
 };
@@ -193,7 +260,7 @@ function initApp() {
       displayMovieDetails();
       break;
     case "/tv-details.html":
-      console.log("shows details");
+      displayTvDetails();
       break;
     case "/search.html":
       console.log("search page");
